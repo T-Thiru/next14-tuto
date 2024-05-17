c1b36bcb70795c12608d9b8f7ce511ab62c3ba82
Thiruvaran Thayalan <110049957+T-Thiru@users.noreply.github.com>
2024-05-17 16:25:01 +0200

    commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,16 @@
+import Footer from "@/components/footer/Footer";
+import Navbar from "@/components/navbar/Navbar";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/navbar/Navbar";
-import Footer from "@/components/footer/Footer";
-import ClientSideProviderTest from "@/components/clientSideProviderTest";
 
 const inter = Inter({ subsets: ["latin"] });
 
+// console.log("hello");
+
 export const metadata = {
   title: {
-    default:"Next.js 14 Homepage",
-    template:"%s | Next.js 14"
+    default: "Next.js 14 Homepage",
+    template: "%s | Next.js 14",
   },
   description: "Next.js starter app description",
 };
@@ -19,11 +20,11 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         {/* <ClientSideProviderTest> */}
-          <div className="container">
-            <Navbar />
-            {children}
-            <Footer />
-          </div>
+        <div className="container">
+          <Navbar />
+          {children}
+          <Footer />
+        </div>
         {/* </ClientSideProviderTest> */}
       </body>
     </html>
